6bdc9ae52b86344ffd1a8fc4438231fab7ad5445
Rezgar Arabzadeh <85969607+rarabzad@users.noreply.github.com>
2023-09-16 01:33:53 -0400

    Update rdrs_ncdf_aggregator.R

--- rdrs_ncdf_aggregator.R
+++ rdrs_ncdf_aggregator.R
@@ -59,99 +59,180 @@
 #' 		     fun = fun,
 #' 		     gp_var = gp_var)
 #' @author Rezgar Arabzadeh, University of Waterloo, July 2023
-rdrs_ncdf_aggregator<-function(ncdir,outdir,outputfile,shift,aggregationLength,var,var_units,var_names,fun,gp_var)
+rdrs_ncdf_aggregator<-function(ncdir=getwd(),
+                               outdir=paste0(ncdir,"/output"),
+                               outputfile="RavenInput.nc",
+                               shift=4,
+                               aggregationLength=24,
+                               var=c("RDRS_v2.1_A_PR0_SFC",
+                                     "RDRS_v2.1_P_TT_1.5m",
+                                     "RDRS_v2.1_P_TT_1.5m",
+                                     "RDRS_v2.1_P_TT_1.5m"),
+                               gp_var="RDRS_v2.1_P_GZ_SFC",
+                               var_units=c("mm",
+                                           "degC",
+                                           "degC",
+                                           "degC"),
+                               var_names=c("precipitation",
+                                           "mean_temperature",
+                                           "min_temperature",
+                                           "max_temperature"),
+                               fun=c("sum",
+                                     "mean",
+                                     "min",
+                                     "max"))
 {
+  if(!dir.exists(ncdir)) stop("NetCDFs input directory doesn't exist!")
+  if(!dir.exists(outdir)) dir.create(outdir)
   ncfiles<-list.files(ncdir,pattern = "*.nc",full.names = T)
-  nc<-nc_open(ncfiles[1])
-  start<-ymd_hms(paste0(gsub(".nc","",basename(ncfiles[1])),"0000"))
-  end<-ymd_hms(paste0(gsub(".nc","",basename(ncfiles[length(ncfiles)])),"0000"))+hours(23)
-  dates<-seq(start,end,"hour")
-  dates_after_shift<-dates-hours(shift)
-  step_size <- 3600 * aggregationLength # hours in seconds
-  aggregation_hourly_dates <- seq(from = range(dates_after_shift)[1], to = range(dates_after_shift)[2], by = step_size)
-  nearest_aggregation_hourly_dates <- floor_date(dates_after_shift, sprintf("%s hour",aggregationLength))
-  grouped_hourly_dates <- split(dates_after_shift, nearest_aggregation_hourly_dates)
-  group_index<-rep(1:length(grouped_hourly_dates),unlist(lapply(grouped_hourly_dates,length)))
-  group_name <-rep(names(grouped_hourly_dates),unlist(lapply(grouped_hourly_dates,length)))
-  files_indices<-data.frame(dates=dates,
-                            dates_after_shift=dates_after_shift,
-                            group_index=group_index,
-                            group_name=group_name,
-                            ncfiles=rep(ncfiles,each=24),
-                            layer=rep(1:24,length(ncfiles)))
-  Dates<-unique(files_indices$group_name)
-  val<-array(NA,c(dim(ncvar_get(nc,var[1]))[1:2],length(Dates),length(var)))
-  for(k in 1:length(unique(var)))
+  if(length(ncfiles)==0) stop("No NetCDF files found in the 'ncdir'")
+  all_vars<-c(var,gp_var)
+  all_vars<-all_vars[nchar(all_vars)>0]
+  file_open_flag<-variable_exist_flag<-rep(F,length(ncfiles))
+  pb <- progress_bar$new(total = length(ncfiles))
+  cat("#######################################\n")
+  cat("######## checking input files #########\n")
+  cat("#######################################\n")
+  for(i in 1:length(ncfiles))
   {
-    cat("#######################################\n")
-    cat(sprintf("### Aggregating: %s ###\n",var[k]))
-    cat("#######################################\n")
-    currentVar<-unique(var)[k]
-    currentVarId<-which(!is.na(match(var,currentVar)))
-    pb <- progress_bar$new(total = length(Dates))
-    for(i in 1:length(Dates))
+    pb$tick()
+    nc<-tryCatch(nc_open(ncfiles[i]),error=function(e) T)
+    if(class(nc)=="ncdf4")
     {
-      currentDate<-Dates[i]
-      id<-which(!is.na(match(files_indices$group_name,currentDate)))
-      currentFiles<-files_indices$ncfiles[id]
-      currentLayers<-files_indices$layer[id]
-      val.tmp<-array(NA,c(dim(ncvar_get(nc,currentVar))[1:2],length(currentVarId),length(unique(currentFiles))))
-      for(j in 1:length(unique(currentFiles)))
-      {
-        nc_j<-nc_open(unique(currentFiles)[j])
-        currentVal<-ncvar_get(nc_j,currentVar)
-        for(l in 1:length(currentVarId))
-        {
-          if(fun[currentVarId[l]]=="sum")  val.tmp[,,l,j]<-rowSums (currentVal[,,currentLayers[which(!is.na(match(currentFiles,unique(currentFiles)[j])))],drop=F], dims=2)
-          if(fun[currentVarId[l]]=="mean") val.tmp[,,l,j]<-rowMeans(currentVal[,,currentLayers[which(!is.na(match(currentFiles,unique(currentFiles)[j])))],drop=F], dims=2)
-          if(fun[currentVarId[l]]=="min")  val.tmp[,,l,j]<-apply   (currentVal[,,currentLayers[which(!is.na(match(currentFiles,unique(currentFiles)[j])))],drop=F], c(1, 2), min)
-          if(fun[currentVarId[l]]=="max")  val.tmp[,,l,j]<-apply   (currentVal[,,currentLayers[which(!is.na(match(currentFiles,unique(currentFiles)[j])))],drop=F], c(1, 2), max)
-        }
-        nc_close(nc_j)
-      }
-      for(l in 1:length(currentVarId))
-      {
-        if(fun[k]=="sum")  val[,,i,k+l-1]<- apply(val.tmp[,,l,,drop=F],c(1,2),sum) *aggregationFactor[k+l-1]
-        if(fun[k]=="mean") val[,,i,k+l-1]<- apply(val.tmp[,,l,,drop=F],c(1,2),mean)*aggregationFactor[k+l-1]
-        if(fun[k]=="min")  val[,,i,k+l-1]<- apply(val.tmp[,,l,,drop=F],c(1,2),min) *aggregationFactor[k+l-1]
-        if(fun[k]=="max")  val[,,i,k+l-1]<- apply(val.tmp[,,l,,drop=F],c(1,2),max) *aggregationFactor[k+l-1]
-      }
-      pb$tick()
+      if(!all(all_vars %in% names(nc$var))) variable_exist_flag[i]<-T
+      nc_close(nc)
+    }else{
+      file_open_flag[i]<-nc
     }
   }
+  if(any(variable_exist_flag))
+  {
+    stop(paste0("one or some of the variables are missing in the following files:\n",
+                paste0(ncfiles[variable_exist_flag],collapse = "\n")))
+  }
+  if(any(file_open_flag))
+  {
+    if(all_vars==gp_var)
+    {
+      warning(paste0("one or some of the nc files are corrputed/cannot be opened:\n",
+                     paste0(ncfiles[file_open_flag],collapse = "\n"),"\nThe corrupted file(s) are ignored!"))
+      
+    }else{
+      stop(paste0("one or some of the nc files are corrputed/cannot be opened:\n",
+                  paste0(ncfiles[file_open_flag],collapse = "\n")))
+    }
+  }
+  nc<-nc_open(ncfiles[1])
   lat<-ncvar_get(nc,"lat")
   lon<-ncvar_get(nc,"lon")
   nlon <- length(nc$dim$rlon$vals)
   nlat <- length(nc$dim$rlat$vals)
   rlat<-seq(range(lat)[1],range(lat)[2],length.out=nlat)
   rlon<-seq(range(lon)[1],range(lon)[2],length.out=nlon)
-  times<-round(seq(aggregationLength*3600/2,
-                   aggregationLength*3600*(length(Dates)-1)+aggregationLength*3600/2,
-                   by=aggregationLength*3600))
-  time_unit<-paste("seconds since",ymd_hms(paste(as.Date(files_indices$dates_after_shift[1]),"00:00:00"))+aggregationLength*3600/2)
   rlon_dim  <- ncdim_def( name = "rlon", units = "degree",vals =rlon)
   rlat_dim  <- ncdim_def( name = "rlat", units = "degree",vals =rlat)
-  time_dim  <- ncdim_def( name = "time", units = time_unit , vals =times)
-  vars<-vector(mode = "list", length = 5+length(var)+ifelse(gp_var=="",0,1))
+  vars<-vector(mode = "list", length = 4 + ifelse(var != "",length(var) + 1,0) + ifelse(gp_var != "",1,0))
   vars[[1]] <- ncvar_def(name = "longitude"   , units = "degree", dim = list(rlon_dim),           missval = NaN,prec="double")
   vars[[2]] <- ncvar_def(name = "latitude"    , units = "degree", dim = list(rlat_dim),           missval = NaN,prec="double")
   vars[[3]] <- ncvar_def(name = "rotated_lon" , units = "degree", dim = list(rlon_dim,rlat_dim),  missval = NaN,prec="double")
   vars[[4]] <- ncvar_def(name = "rotated_lat" , units = "degree", dim = list(rlon_dim,rlat_dim),  missval = NaN,prec="double")
-  vars[[5]] <- ncvar_def(name = "times"       , units = time_unit,dim = list(time_dim)        ,   missval = NaN,prec="double")
-  for(i in 1:length(var))
+  
+  if(var != "")
   {
-    vars[[5+i]] <- ncvar_def(name = var_names[i],
-                             units = var_units[i] ,
-                             dim = list(rlon_dim,rlat_dim,time_dim),
-                             missval = NaN,
-                             prec="double")
+    start<-ymd_hms(paste0(gsub(".nc","",basename(ncfiles[1])),"0000"))
+    end<-ymd_hms(paste0(gsub(".nc","",basename(ncfiles[length(ncfiles)])),"0000"))+hours(23)
+    dates<-seq(start,end,"hour")
+    dates_after_shift<-dates-hours(shift)
+    step_size <- 3600 * aggregationLength # in seconds
+    aggregation_hourly_dates <- seq(from = range(dates_after_shift)[1], to = range(dates_after_shift)[2], by = step_size)
+    nearest_aggregation_hourly_dates <- floor_date(dates_after_shift, sprintf("%s hour",aggregationLength))
+    grouped_hourly_dates <- split(dates_after_shift, nearest_aggregation_hourly_dates)
+    group_index<-rep(1:length(grouped_hourly_dates),unlist(lapply(grouped_hourly_dates,length)))
+    group_name <-rep(names(grouped_hourly_dates),unlist(lapply(grouped_hourly_dates,length)))
+    files_indices<-data.frame(dates=dates,
+                              dates_after_shift=dates_after_shift,
+                              group_index=group_index,
+                              group_name=group_name,
+                              ncfiles=rep(ncfiles,each=24),
+                              layer=rep(1:24,length(ncfiles)))
+    Dates<-unique(files_indices$group_name)
+    val<-array(NA,c(dim(ncvar_get(nc,var[1]))[1:2],length(Dates),length(var)))
+    times<-round(seq(aggregationLength*3600/2,
+                     aggregationLength*3600*(length(Dates)-1)+aggregationLength*3600/2,
+                     by=aggregationLength*3600))
+    time_unit<-paste("seconds since",ymd_hms(paste(as.Date(files_indices$dates_after_shift[1]),"00:00:00"))+aggregationLength*3600/2)
+    time_dim  <- ncdim_def( name = "time", units = time_unit , vals =times)
+    vars[[5]] <- ncvar_def(name = "times"       , units = time_unit,dim = list(time_dim)        ,   missval = NaN,prec="double")
+    for(k in 1:length(unique(var)))
+    {
+      cat("#######################################\n")
+      cat(sprintf("### Aggregating: %s ###\n",var[k]))
+      cat("#######################################\n")
+      currentVar<-unique(var)[k]
+      currentVarId<-which(!is.na(match(var,currentVar)))
+      pb <- progress_bar$new(total = length(Dates))
+      for(i in 1:length(Dates))
+      {
+        currentDate<-Dates[i]
+        id<-which(!is.na(match(files_indices$group_name,currentDate)))
+        currentFiles<-files_indices$ncfiles[id]
+        currentLayers<-files_indices$layer[id]
+        val.tmp<-array(NA,c(dim(ncvar_get(nc,currentVar))[1:2],length(currentVarId),length(unique(currentFiles))))
+        for(j in 1:length(unique(currentFiles)))
+        {
+          nc_j<-nc_open(unique(currentFiles)[j])
+          currentVal<-ncvar_get(nc_j,currentVar)
+          for(l in 1:length(currentVarId))
+          {
+            if(fun[currentVarId[l]]=="sum")  val.tmp[,,l,j]<-rowSums (currentVal[,,currentLayers[which(!is.na(match(currentFiles,unique(currentFiles)[j])))],drop=F], dims=2)
+            if(fun[currentVarId[l]]=="mean") val.tmp[,,l,j]<-rowMeans(currentVal[,,currentLayers[which(!is.na(match(currentFiles,unique(currentFiles)[j])))],drop=F], dims=2)
+            if(fun[currentVarId[l]]=="min")  val.tmp[,,l,j]<-apply   (currentVal[,,currentLayers[which(!is.na(match(currentFiles,unique(currentFiles)[j])))],drop=F], c(1, 2), min)
+            if(fun[currentVarId[l]]=="max")  val.tmp[,,l,j]<-apply   (currentVal[,,currentLayers[which(!is.na(match(currentFiles,unique(currentFiles)[j])))],drop=F], c(1, 2), max)
+          }
+          nc_close(nc_j)
+        }
+        for(l in 1:length(currentVarId))
+        {
+          if(fun[k]=="sum")  val[,,i,k+l-1]<- apply(val.tmp[,,l,,drop=F],c(1,2),sum) *aggregationFactor[k+l-1]
+          if(fun[k]=="mean") val[,,i,k+l-1]<- apply(val.tmp[,,l,,drop=F],c(1,2),mean)*aggregationFactor[k+l-1]
+          if(fun[k]=="min")  val[,,i,k+l-1]<- apply(val.tmp[,,l,,drop=F],c(1,2),min) *aggregationFactor[k+l-1]
+          if(fun[k]=="max")  val[,,i,k+l-1]<- apply(val.tmp[,,l,,drop=F],c(1,2),max) *aggregationFactor[k+l-1]
+        }
+        pb$tick()
+      }
+    }
+    for(i in 1:length(var))
+    {
+      vars[[5+i]] <- ncvar_def(name = var_names[i],
+                               units = var_units[i] ,
+                               dim = list(rlon_dim,rlat_dim,time_dim),
+                               missval = NaN,
+                               prec="double")
+    }
   }
   if(gp_var != "")
   {
-    gph<-ncvar_get(nc_open(ncfiles[1]),gp_var)[,,1]
+    cat("#######################################\n")
+    cat(sprintf("### Aggregating: %s ###\n",gp_var))
+    cat("#######################################\n")
+    id<-which(!variable_exist_flag & !file_open_flag)
+    nc<-nc_open(ncfiles[id[1]])
+    gph<-array(NA,dim(ncvar_get(nc,gp_var))*c(1,1,length(id)/24))
+    nc_close(nc)
+    j<-0
+    pb <- progress_bar$new(total = length(id))
+    for(i in id)
+    {
+      j<-j+1
+      nc<-nc_open(ncfiles[i])
+      gph[,,j]<-apply(ncvar_get(nc,gp_var),c(1,2),mean)
+      nc_close(nc)
+      pb$tick()
+    }
+    gph<-apply(gph,c(1,2),mean)
     geo2ele<-function(gph)  (gph*10*9.81)*6371000/(9.81*6371000-gph*10*9.81)
     gpe<-geo2ele(gph)
-    vars[[6+i]] <- ncvar_def(name = "Geopotential_Elevation",
+    vars[[length(vars)]] <- ncvar_def(name = "Geopotential_Elevation",
                              units = "MASL",
                              dim = list(rlon_dim,rlat_dim),
                              missval = NaN,
@@ -163,14 +244,17 @@ rdrs_ncdf_aggregator<-function(ncdir,outdir,outputfile,shift,aggregationLength,v
   ncvar_put( ncnew, vars[[2]],  rlat,  start=1,      count=length(rlat))
   ncvar_put( ncnew, vars[[3]],  lon,   start=c(1,1), count=dim(lon))
   ncvar_put( ncnew, vars[[4]],  lat,   start=c(1,1), count=dim(lat))
-  ncvar_put( ncnew, vars[[5]],  times, start=1,      count=length(Dates))
-  for(i in 1:length(var))
+  if(var != "")
   {
-    ncvar_put( ncnew, vars[[i+5]], val[,,,i], start=c(1,1,1), count=dim(val[,,,i]))
+    ncvar_put( ncnew, vars[[5]],  times, start=1,      count=length(Dates))
+    for(i in 1:length(var))
+    {
+      ncvar_put( ncnew, vars[[i+5]], val[,,,i], start=c(1,1,1), count=dim(val[,,,i]))
+    }
   }
   if(gp_var != "")
   {
-    ncvar_put( ncnew, vars[[i+6]], gpe, start=c(1,1), count=dim(gpe))
+    ncvar_put( ncnew, vars[[length(vars)]], gpe, start=c(1,1), count=dim(gpe))
   }
   nc_close(ncnew)
 }
